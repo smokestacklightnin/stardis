f151bcedd6b39e67115afd626fa84a8fb01ed320
smokestacklightnin <smokestacklightnin@users.noreply.github.com>
2023-06-27 05:09:17 +0000

    deploy: 625d28c14be9799389247bede3ec3376b677840e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gbee65aa',
+    VERSION: '0.1.dev1+g625d28c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
