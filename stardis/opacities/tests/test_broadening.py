2bbe9637366f41b4941d6f4c3511444e24fb489c
William Black <125844868+smokestacklightnin@users.noreply.github.com>
2023-08-10 19:41:07 -0700

    Add `_calc_gamma_linear_stark_cuda()`

--- stardis/opacities/tests/test_broadening.py
+++ stardis/opacities/tests/test_broadening.py
@@ -8,6 +8,7 @@ from stardis.opacities.broadening import (
     _calc_doppler_width_cuda,
     calc_doppler_width_cuda,
     calc_gamma_linear_stark,
+    _calc_gamma_linear_stark_cuda,
 )
 
 GPUs_available = cuda.is_available()
@@ -161,4 +162,45 @@ def test_calc_gamma_linear_stark_sample_values(
             calc_gamma_linear_stark_sample_values_input_electron_density,
         ),
         calc_gamma_linear_stark_sample_values_expected_result,
+    )
+
+
+@pytest.mark.skipif(
+    not GPUs_available, reason="No GPU is available to test CUDA function"
+)
+@pytest.mark.parametrize(
+    "calc_gamma_linear_stark_cuda_unwrapped_sample_values_input_n_eff_upper,calc_gamma_linear_stark_cuda_unwrapped_sample_values_input_n_eff_lower,calc_gamma_linear_stark_cuda_unwrapped_sample_values_input_electron_density,calc_gamma_linear_stark_cuda_unwrapped_sample_values_expected_result",
+    [
+        (
+            np.array(2 * [1]),
+            np.array(2 * [0]),
+            np.array(2 * [(0.51 * 0.642) ** (-3 / 2)]),
+            np.array(2 * [1.0]),
+        ),
+    ],
+)
+def test_calc_gamma_linear_stark_cuda_unwrapped_sample_values(
+    calc_gamma_linear_stark_cuda_unwrapped_sample_values_input_n_eff_upper,
+    calc_gamma_linear_stark_cuda_unwrapped_sample_values_input_n_eff_lower,
+    calc_gamma_linear_stark_cuda_unwrapped_sample_values_input_electron_density,
+    calc_gamma_linear_stark_cuda_unwrapped_sample_values_expected_result,
+):
+    arg_list = (
+        calc_gamma_linear_stark_cuda_unwrapped_sample_values_input_n_eff_upper,
+        calc_gamma_linear_stark_cuda_unwrapped_sample_values_input_n_eff_lower,
+        calc_gamma_linear_stark_cuda_unwrapped_sample_values_input_electron_density,
+    )
+
+    arg_list = tuple(map(cp.array, arg_list))
+    result_values = cp.empty_like(arg_list[0], dtype=float)
+
+    nthreads = 256
+    length = len(calc_gamma_linear_stark_cuda_unwrapped_sample_values_expected_result)
+    nblocks = 1 + (length // nthreads)
+
+    _calc_gamma_linear_stark_cuda[nblocks, nthreads](result_values, *arg_list)
+    print(result_values)
+    assert np.allclose(
+        cp.asnumpy(result_values),
+        calc_gamma_linear_stark_cuda_unwrapped_sample_values_expected_result,
     )
